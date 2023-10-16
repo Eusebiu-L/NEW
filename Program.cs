a97fe40e561c35e242ce15ac071b0371517a9697
Eusebiu-L <146477522+Eusebiu-L@users.noreply.github.com>
2023-10-16 08:05:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace suma2nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
